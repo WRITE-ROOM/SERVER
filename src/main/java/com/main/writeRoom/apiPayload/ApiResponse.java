7e2b2bf2d8797bc541499d2fffb27c40fc9571b2
jhsong76 <20190976@sungshin.ac.kr>
2024-02-02 20:06:18 +0900

    fd

--- src/main/java/com/main/writeRoom/apiPayload/ApiResponse.java
+++ src/main/java/com/main/writeRoom/apiPayload/ApiResponse.java
@@ -30,7 +30,7 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> of(BaseCode code, T result) {
         return new ApiResponse<>(true, code.getReasonHttpStatus().getCode() , code.getReasonHttpStatus().getMessage(), result);
     }
-
+efd
     public static <T> ApiResponse<T> onFailure(String code, String message, T data){
         return new ApiResponse<>(false, code, message, data);
     }
